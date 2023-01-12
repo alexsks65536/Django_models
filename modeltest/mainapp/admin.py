a51143f4c0b3437de42fbfb25606a109f7902a9f
alexsks65536 <megbyte@mail.ru>
2023-01-12 23:49:18 +0300

    Person models

--- modeltest/mainapp/admin.py
+++ modeltest/mainapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'shirt_size')
+    list_display_links = ('id', 'name', 'shirt_size')
+    search_fields = ('name', 'shirt_size')
+
+
+admin.site.register(Person, PersonAdmin)
+
+
+admin.site.site_title = 'Админ-панель "Изучение Django"'
+admin.site.site_header = 'Админ-панель "Изучение Django"'
--- modeltest/mainapp/models.py
+++ modeltest/mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+"""
+Последовательность из двух кортежей для выбора в этом поле. Если это задано, виджет формы по умолчанию будет 
+представлять собой поле выбора вместо стандартного текстового поля и будет ограничивать выбор заданными вариантами. 
+"""
 
-# Create your models here.
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
